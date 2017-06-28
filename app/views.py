258855d43e959599588de5761a1d8fe9c0454296
Dhruv Arora <dhruvarora@delhinerds.com>
2017-06-28 13:34:31 +0530

    added templates and login screen

--- app/views.py
+++ app/views.py
@@ -2,5 +2,27 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
 
-# Create your views here.
+def index(request):
+    if request.method == "POST":
+        username = request.POST.get("name")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return render(request, "user-index.html", {})
+            else:
+                return render(request, "login.html", {
+                "message":"This account has been blocked. Please contact support@smileycolor.com."
+                })
+        else:
+            return render(request, "login.html", {
+            "message":"User account does not exist"
+            })
+    else:
+        if request.user.is_authenticated:
+            return render(request, "user-index.html", {})
+        else:
+            return render(request, "login.html", {})
