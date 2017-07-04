ce14a7055fd03a6b849d7aad091d84ff5a5104f2
Arjun Gupta <saimzn4241@gmail.com>
2017-07-04 11:39:49 +0530

    admin

--- app/admin.py
+++ app/admin.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from django.contrib.auth.models import User
 
-from .models import Brand, UserProfile
+from .models import *
 
 class BrandAdmin(admin.ModelAdmin):
     list_display = ["name", "date_added"]
@@ -24,3 +24,28 @@ class UserProfileAdminExtended(admin.ModelAdmin):
 admin.site.register(Brand, BrandAdmin)
 admin.site.unregister(User)
 admin.site.register(User, UserProfileAdminExtended)
+
+admin.site.register(Setting)
+admin.site.register(MemberType)
+admin.site.register(FileCategory)
+admin.site.register(FileRepository)
+admin.site.register(Member)
+admin.site.register(MemberProperty)
+admin.site.register(Evaluation)
+admin.site.register(MemberPreference)
+#   admin.site.register(Brand)
+admin.site.register(MemberEvaluation)
+admin.site.register(PatchTolerance)
+admin.site.register(Standard)
+admin.site.register(Product)
+admin.site.register(Division)
+#admin.site.register(UserProfile)
+admin.site.register(PatchStandard)
+admin.site.register(Job)
+admin.site.register(Device)
+admin.site.register(UserDivision)
+admin.site.register(UserPreference)
+admin.site.register(JobPatch)
+admin.site.register(Sheet)
+admin.site.register(Reading)
+admin.site.register(SheetEvaluation)
